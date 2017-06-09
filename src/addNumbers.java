ba7a7faa16f4bfbcfcf8ffe9eade64ce076951c0
Geccles <geccles01@gmail.com>
2017-06-08 21:52:40 -0400

    printing name and adding

---
+++ src/addNumbers.java
@@ -0,0 +1,18 @@
+/**
+ * Class to add some numbers
+ */
+public class addNumbers {
+
+	public static void main(String[] args) {
+		/**
+		 * Add some numbers together 
+		 * and bonus let's add fancy concatenation to it
+		 * */
+		
+		int x = 74;
+		int y = 36;
+		 
+		System.out.println(x + " + " + y + " = " + (x + y));
+	}
+
+}
---
+++ src/printName.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+/**
+ * Class to print out a name
+ */
+public class printName {
+
+	public static void main(String[] args) {
+		/**
+		 * Write a Java Program to print "Hello" on
+		 * a screen and then print your name on a separate line
+		 */
+
+		//my idea
+		System.out.printf("Hello" + "%n" + "Galina Eccles" + "%n");
+		
+		//solution offered by course: I prefer this, cleaner and less to write
+		System.out.println("\nHello\nGalina Eccles!");
+		
+		//Lets get fancy and have some fun with this
+		System.out.println("\n \nEnter your Name and press the Enter Key ");
+		Scanner input = new Scanner(System.in);
+		String name = input.nextLine();
+		System.out.println("Hello \n" + name);
+	}
+
+}

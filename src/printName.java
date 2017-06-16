dc0042fa283b40aa25ff9bbffd173be799898724
Geccles <geccles01@gmail.com>
2017-06-15 21:15:33 -0400

    add close

--- src/printName.java
+++ src/printName.java
@@ -11,18 +11,18 @@ public class printName {
 		 */
 
 		//my idea
-		//System.out.printf("Hello" + "%n" + "Galina Eccles" + "%n");
+		System.out.printf("Hello" + "%n" + "Galina Eccles" + "%n");
 		
 		//solution offered by course: I prefer this, cleaner and less to write
-		//System.out.println("\nHello\nGalina Eccles!");
+		System.out.println("\nHello\nGalina Eccles!");
 		
 		//Lets get fancy and have some fun with this
-		System.out.println("\n \nWho are you? ");
+		System.out.println("\n \nEnter your Name and press the Enter Key ");
 		Scanner input = new Scanner(System.in);
 		
 		try{
 			String name = input.nextLine();
-			System.out.println("Hello " + name + ", stop man spreading");
+			System.out.println("Hello \n" + name);
 		}finally{
 			//close the scanner
 			input.close();
